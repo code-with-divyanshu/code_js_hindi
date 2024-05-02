ce6aa542fe7bb649c84b7d8122ad3ecee9c1d102
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-05-02 11:13:00 +0530

    Add Intial File

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+// singleton
+// Object.create;
+
+// objects literals
+const mySym = Symbol("key1");
+
+const jsUser = {
+  name: "Divyanshu",
+  "full name": "Divyanshu Bartwal",
+  [mySym]: "India", // use squre bracet to refer symbol
+  age: 23,
+  location: "Dehradun",
+  email: "divyanshubartwal67@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Tuesday"],
+};
+
+console.log(jsUser.email); // mostly  use
+console.log(jsUser["email"]); // some spacial case use
+console.log(jsUser["full name"]);
+console.log(jsUser[mySym]);
+
+jsUser.isLoggedIn = true;
+console.log(jsUser.isLoggedIn);
+
+// Object.freeze(jsUser); // to stop changes in objects
+jsUser.email = "karan@gmail.com";
+console.log(jsUser);
+
+jsUser.greeting = function () {
+  console.log("Hello Js User");
+};
+
+jsUser.greetingTwo = function () {
+  console.log(`Hello Js User, ${this.name}`); // for refer value
+};
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
