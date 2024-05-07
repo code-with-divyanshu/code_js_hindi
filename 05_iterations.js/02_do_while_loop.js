562efcc5d48fa55385f513f384be3b202ae2f2cf
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-05-07 21:17:19 +0530

    Add Initial File

---
+++ 05_iterations.js/02_do_while_loop.js
@@ -0,0 +1,29 @@
+//while loop
+
+// while (condition) {} // syntax
+
+let i = 0;
+while (i <= 10) {
+  console.log(`Value of index is: ${i}`);
+  i++;
+}
+
+let myArr = ["Flash", "Batman", "Superman"];
+let j = 0;
+while (j < myArr.length) {
+  console.log(myArr[j]);
+  j++;
+}
+
+// do-while loop
+
+// do {} while (condition); //syntax
+
+let score = 11;
+
+do {
+  console.log(`Score is: ${score}`);
+  score++;
+} while (score <= 10); // run atleast one time
+
+// first excuted than  check statement
