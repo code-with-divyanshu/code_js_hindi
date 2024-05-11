06b6f25d2410e20f87f4c68a34ab7d66117e0315
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-05-11 10:46:59 +0530

    Add Initial File

---
+++ 05_iterations.js/05_for_each_loop.js
@@ -0,0 +1,52 @@
+const coding = ["js", "cpp", "rb", "py", "html", "css"];
+
+// for each function in array
+
+// simple function
+coding.forEach((val) => {
+  // use function without name
+  //   console.log(val);
+});
+
+// callback  function
+coding.forEach((val) => {
+  // use function without name
+  //   console.log(val);
+});
+
+// declare function first
+
+function printMe(val) {
+  console.log(val);
+}
+
+// coding.forEach(printMe);
+
+// other parameters in for each loop
+
+coding.forEach((val, idx, arr) => {
+  //   console.log(val, idx, arr);
+});
+
+// for each in (objects in array)
+
+const myCoding = [
+  {
+    languageName: "JavaScript",
+    languageFileName: "js",
+  },
+
+  {
+    languageName: "Python",
+    languageFileName: "py",
+  },
+
+  {
+    languageName: "Java",
+    languageFileName: "java",
+  },
+];
+
+myCoding.forEach((item) => {
+  console.log(item.languageName);
+});
