5d48ce9e87d7cea7f409ae7a72741d12e8cd6802
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-05-14 20:05:28 +0530

    Add Initial File

---
+++ 07_projects/01_color-changer/script.js
@@ -0,0 +1,26 @@
+let buttons = document.querySelectorAll(".button");
+let body = document.querySelector("body");
+
+buttons.forEach((btn) => {
+  console.log(btn);
+
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+    }
+
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
